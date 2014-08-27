f4280add66cc55e52c1d8cc2e586920ffe662f15
charlyy <cyoon90@yahoo.com>
2014-08-26 23:46:11 -0700

    navbar added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Testapp::Application.routes.draw do
+  get "posts/index"
+  get "welcome/index"
+  devise_for :users
+  resources :posts
+  resources :welcome
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
